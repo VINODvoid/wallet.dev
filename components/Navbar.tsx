11572f00e7f1203f5f566d5a84fca45cea1553fd
VINODvoid <iamlearner.v@gmail.com>
2025-05-28 22:31:43 +0530

    chore: update dependencies and add new packages
    - Added @solana/web3.js, bs58, ed25519-hd-key, ethers, and tweetnacl to dependencies in package.json
    - Added a new logo.svg file to the public directory

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Logo from "./icons/Logo";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="h-[10dvh] flex justify-between items-center py-4">
+      <div className="flex  items-center gap-2">
+        <Logo />
+        <div className="flex flex-col">
+          <span className="items-center text-3xl font-extrabold text-primary flex gap-2">
+            Wallet.dev{" "}<span className="rounded-full text-base bg-slate-100 px-2">v1.1</span>
+          </span>
+        </div>
+      </div>
 
-export default Navbar
+      <p>topggle theme</p>
+    </nav>
+  );
+};
+
+export default Navbar;
--- components/WalletGenerator.tsx
+++ components/WalletGenerator.tsx
@@ -1,7 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { mnemonicToSeed, generateMnemonic, validateMnemonic, mnemonicToSeedSync } from "bip39";
+import {  generateMnemonic, validateMnemonic, mnemonicToSeedSync } from "bip39";
+import { derivePath } from "ed25519-hd-key";
 import axios from "axios";
+import nacl from "tweetnacl";
+import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
+import { ethers } from "ethers";
+import { Mnemonic } from "ethers";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { log } from "console";
 
 interface Wallet {
   public_key: string;
@@ -9,6 +18,10 @@ interface Wallet {
   mnemonic: string;
   path: string;
 }
+
+
+
+
 const WalletGenerator = () => {
   const pathNamesTypes: { [key: string]: string } = {
     "501": "Solana",
@@ -24,13 +37,24 @@ const WalletGenerator = () => {
   const generateWallet = () => {
     let mnemonic = mnemonicInput.trim();
 
-    if (mnemonic) {
-      if (!validateMnemonic(mnemonic)) {
-        <div>Secret Phase is not that Secret</div>;
-      } else {
-        mnemonic = generateMnemonic();
-      }
+    if(mnemonic && !validateMnemonic(mnemonic))
+    {
+      alert("Secret Phase is not that Secret");
+      return;
     }
+    else if(!mnemonic)
+    {
+      mnemonic = generateMnemonic();
+    }
+
+
+    // if (mnemonic) {
+    //   if (!validateMnemonic(mnemonic)) {
+    //     <div>Secret Phase is not that Secret</div>;
+    //   } else {
+    //     mnemonic = generateMnemonic();
+    //   }
+    // }
     let words = mnemonic.split(" ");
     SetMnemonicWords(words);
 
@@ -44,33 +68,85 @@ const WalletGenerator = () => {
     pathType: string,
     mnemonic: string,
     accountCount: number
-  ) => {
+  ) :Wallet | null=> {
         try {
             const seedBuffer = mnemonicToSeedSync(mnemonic);
             const path = `m/44'/${pathType}'/0'/${accountCount}'`;
+            const {key:derivedSeed} = derivePath(path,seedBuffer.toString("hex"));
+
+            let publicKeyEncoded:string| undefined;
+            let privateKeyEncoded:string | undefined;
+            if(pathType === "501")
+            {
+              const {secretKey} = nacl.sign.keyPair.fromSeed(derivedSeed);
+              const  keypair = Keypair.fromSecretKey(secretKey);
+
+              privateKeyEncoded = bs58.encode(secretKey);
+              publicKeyEncoded = keypair.publicKey.toBase58();
+            }
+            else if (pathType === "60")
+            {
+              const privateKey= Buffer.from(derivedSeed).toString("hex");
+              privateKeyEncoded = privateKey;
+
+
+              const wallet = new ethers.Wallet(privateKey);
+
+              publicKeyEncoded = wallet.address;
+            }
+          else{
+            alert("Error in creating Wallet")
+          }
+          if(!publicKeyEncoded || !privateKeyEncoded)
+          {
+            alert("No creation")
+            return null;
+          }
+          return {
+            public_key:publicKeyEncoded,
+            private_key:privateKeyEncoded,
+            mnemonic:mnemonic,
+            path:path
             
+          };
         } catch (error) {
-            
+
+          alert("Error in trycatch")
+            return null;
         }
   };
+  
   return (
-    <div className="min-h-screen max-w-dvw">
-      <div>Generate Wallet , choose the blockchain to start </div>
-      <button onClick={() => setPathNames(["501"])}>Solana</button>
-      <button onClick={() => setPathNames(["60"])}>Ethereum</button>
+    <div className="h-[90dvh] max-w-screen">
+        <p>Generate Wallet , choose the blockchain to start </p>
+      <div className="flex p-4 m-4 justify-center">
 
+      <Button onClick={() => setPathNames(["501"])} className="m-4 hover:bg-slate-100" variant={"outline"}>Solana</Button>
+      <Button onClick={() => setPathNames(["60"])} variant={"outline"} className="m-4 hover:bg-slate-100">Ethereum</Button>
+</div>
       {pathNames.length != 0 && (
         <div>
           <h1 className="font-mono">Generate Secret Phrase </h1>
-          <input
+          <Input
             type="text"
             placeholder="Add your Secret phrase or leave blank to generate"
             onChange={(e) => SetMnemonicInput(e.target.value)}
             value={mnemonicInput}
           />
-          <button onClick={generateWallet}>Generate Wallet</button>
+          <Button onClick={generateWallet} className="" variant={"secondary"}>Generate Wallet</Button>
         </div>
       )}
+      {
+        mnemonicWords && (
+          <div className="">
+            {mnemonicWords.join(" ")}
+
+          </div>
+        )
+      }
+
+      
+      
     </div>
   );
 };
