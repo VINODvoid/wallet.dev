0a7b20488a91a4ce1708ecc6d4214d646bc6a3a6
VINODvoid <iamlearner.v@gmail.com>
2025-06-11 07:59:09 +0530

    feat: add Separator and Switch components; implement ModeToggle for theme switching
    - Created a new Separator component using Radix UI for consistent UI separation.
    - Implemented a Switch component for toggling states, styled with Tailwind CSS.
    - Added ModeToggle component to switch between light and dark themes using next-themes.
    - Updated package.json and package-lock.json to include new dependencies for Radix UI components and framer-motion.

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <main className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[92vh]">
     <Navbar/>
     <WalletGenerator/>
-    </>
+    </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Logo from "./icons/Logo";
+import { ModeToggle } from "./ui/theme-button";
 
 const Navbar = () => {
   return (
-    <nav className="h-[10dvh] flex justify-between items-center py-4">
+    <nav className="flex justify-between items-center py-4">
       <div className="flex  items-center gap-2">
         <Logo />
         <div className="flex flex-col">
-          <span className="items-center text-3xl font-extrabold text-primary flex gap-2">
-            Wallet.dev{" "}<span className="rounded-full text-base bg-slate-100 px-2">v1.1</span>
+          <span className="tracking-tighter items-center text-3xl font-extrabold text-primary flex gap-2 ">
+            Wallet.dev{" "}<span className="rounded-full text-base bg-primary/10 border border-primary/50 px-2">v1.1</span>
           </span>
         </div>
       </div>
 
-      <p>topggle theme</p>
+      <ModeToggle/>
     </nav>
   );
 };
